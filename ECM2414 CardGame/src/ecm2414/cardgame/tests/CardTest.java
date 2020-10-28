9af04908fd5348c596a2addf844494d147a05d55
Alex Rundle <alexrundle01@gmail.com>
2020-10-28 13:18:29 +0000

    added Card constructor test

--- ECM2414 CardGame/src/ecm2414/cardgame/tests/CardTest.java
+++ ECM2414 CardGame/src/ecm2414/cardgame/tests/CardTest.java
@@ -1,5 +1,19 @@
 package ecm2414.cardgame.tests;
 
-public class CardTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import ecm2414.cardgame.Card;
+
+public class CardTest {
+		
+	@Test
+	void testCard()
+	{
+		int denom = 1;
+		Card card = new Card(denom);
+		
+		assertEquals(card.denomination, denom);
+	}
 }
